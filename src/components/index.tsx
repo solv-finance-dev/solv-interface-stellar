53992715bb3d47213dc0cb42a04edf9735c2e4a1
zewei.zhang <zewei@57blocks.com>
2025-08-25 11:07:16 +0800

    fix: remove useless code

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,5 @@
 export * as Accordion from './Accordion';
 export * as Banner from './Banner';
-export { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from './Dialog';
 export * as FAQ from './FAQ';
 export * as Header from './Header';
 export * as Pagination from './Pagination';
