3dd3e22e9a22c82730801a74431fdb66e6fe8eb6
zewei.zhang <zewei@57blocks.com>
2025-08-29 14:52:12 +0800

    feat: add toaster

--- src/providers/BodyProvider.tsx
+++ src/providers/BodyProvider.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { ReactNode } from 'react';
+import { Toaster } from '@solvprotocol/ui-v2';
 
 const BodyProvider = ({ children }: { children: ReactNode }) => {
   return (
     <div className='mx-auto max-w-[1200px] pt-[5.75rem] lg:pt-[6.5rem]'>
       {children}
+
+      <Toaster />
     </div>
   );
 };
